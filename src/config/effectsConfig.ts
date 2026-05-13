bd6c732e04d745075ca2cbe0b4d0c90adc9e73dc
Lynn7777-0309 <3579876737@qq.com>
2026-05-13 14:02:50 +0800

    musicsakura

--- src/config/effectsConfig.ts
+++ src/config/effectsConfig.ts
@@ -4,10 +4,10 @@ import type { SakuraConfig } from "../types/config";
 
 export const sakuraConfig: SakuraConfig = {
 	// 是否启用樱花特效
-	enable: false,
+	enable: true,
 
 	// 是否允许用户在设置中切换
-	switchable: true,
+	switchable: false,
 
 	// 樱花数量
 	sakuraNum: 21,
