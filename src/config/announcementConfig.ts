7b9ec0239ede978ee71d682489c264960d9cdaec
Lynn7777-0309 <3579876737@qq.com>
2026-05-13 14:05:00 +0800

    公告

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,7 +5,7 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "欢迎来到我的博客！请想象这里有个小人在鞠躬。",
 
 	// 是否允许用户关闭公告
 	closable: true,
